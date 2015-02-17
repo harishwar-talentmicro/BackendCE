22fc2f6be3ca5ac8fa8c0cda4d0e85b603e4dae2
krunal patel <krunal.patel@hirecraft.in>
2015-02-17 18:36:57 +0530

    commit final changes

--- public/js/Disclaimer.js
+++ public/js/Disclaimer.js
@@ -7,7 +7,7 @@
         }
         else {
             if (typeof (Storage) !== "undefined") {
-                var encrypted = sessionStorage.getItem("_token");
+                var encrypted = localStorage.getItem("_token");
                 if (encrypted) {
                     var decrypted = CryptoJS.AES.decrypt(encrypted, "EZEID");
                     var Jsonstring = decrypted.toString(CryptoJS.enc.Utf8);
