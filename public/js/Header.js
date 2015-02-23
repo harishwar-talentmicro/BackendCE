89edafbe9ee0dec26c5fe94db7440bd43ea55682
krunal patel <krunal.patel@hirecraft.in>
2015-02-23 20:30:04 +0530

    commit final changes

--- public/js/Header.js
+++ public/js/Header.js
@@ -21,7 +21,7 @@ HeaderApp.directive('headerSection', function (Notification) {
                     }).success(function (data,status,x) {
                         $rootScope._userInfo = data;
 
-                           
+
 
                         if (typeof (Storage) !== "undefined") {
                             var encrypted = CryptoJS.AES.encrypt(JSON.stringify(data), "EZEID");
