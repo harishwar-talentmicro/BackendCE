d0c8ed57b219b6dbd305b2d2924c7ee1556d93bb
indrajeet <indrajeet@hirecraft.in>
2015-03-07 18:50:53 +0530

    Loading items and adding items completely done Displaying items is going on

--- public/js/App.js
+++ public/js/App.js
@@ -58,6 +58,29 @@
     var MsgDelay = 2000;
 
     /**
+     * Number only directive (allows only number for input fields)
+     */
+    ezeid.directive('numbersOnly', function(){
+        return {
+            require: 'ngModel',
+            link: function(scope, element, attrs, modelCtrl) {
+                modelCtrl.$parsers.push(function (inputValue) {
+                    // this next if is necessary for when using ng-required on your input.
+                    // In such cases, when a letter is typed first, this parser will be called
+                    // again, and the 2nd time, the value will be undefined
+                    if (inputValue == undefined) return ''
+                    var transformedInput = inputValue.replace(/[^0-9]/g, '');
+                    if (transformedInput!=inputValue) {
+                        modelCtrl.$setViewValue(transformedInput);
+                        modelCtrl.$render();
+                    }
+
+                    return transformedInput;
+                });
+            }
+        };
+    });
+    /**
      * Directive to capitalize the input field
      */
     ezeid.directive('capitalize', function() {
