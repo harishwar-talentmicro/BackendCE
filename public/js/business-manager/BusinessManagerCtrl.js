e9179b6640bf3bfe0051c070635323d708091389
indrajeet <indrajeet@hirecraft.in>
2015-03-17 18:36:17 +0530

    Implemented tab controls and access controls for users and subusers Integrated UI Grid for showing items Implemented color scheme for UI Grid

--- public/js/business-manager/BusinessManagerCtrl.js
+++ public/js/business-manager/BusinessManagerCtrl.js
@@ -4,14 +4,44 @@
  */
 angular.module('ezeidApp').controller('BusinessManagerCtrl',['$scope','$rootScope','MsgDelay','GURL','$http','$interval','Notification',function($scope,$rootScope,MsgDelay,GURL,$http,$interval,Notification){
 
+    $scope.templates = [
+        'html/business-manager/sales/sales.html',
+        'html/business-manager/reservation/reservation.html',
+        'html/business-manager/home-delivery/home-delivery.html',
+        'html/business-manager/service/service.html',
+        'html/business-manager/resume/resume.html'
+    ];
+
     /**
      * List of Modules
-     * title : User Custom Title
-     * enabled : User enabled from settings or not
-     * listType : List Type selected for each module in setttings
      * @type {Array}
      */
+    $scope.modules = [];
 
+    /**
+     * Index of Current Active Module Selected
+     * @type {number}
+     */
+    $scope.selectedModule = 0;
+    $scope.currentTemplate = 'html/business-manager/loading.html';
+    /**
+     * Selecting a module and loading it's view
+     * @param moduleIndex
+     */
+    $scope.selectModule = function(moduleIndex){
+        var index = parseInt(moduleIndex.moduleIndex);
+        $scope.selectedModule = index;
+        $scope.resetSelectedModules();
+        $scope.modules[index].cssClass = 'active';
+        $scope.currentTemplate = $scope.templates[$scope.modules[index].type];
+        console.log($scope.currentTemplate);
+    };
+
+    $scope.resetSelectedModules = function(){
+        for(var i = 0; i < $scope.modules.length; i++){
+            $scope.modules[i].cssClass = '';
+        }
+    };
     /**
      * Module (Prototype)
      * @param type => (Integer) 0: Sales, 1 : Reservation, 2 : Home Delivery, 3 : Service, 4 : Resume
@@ -21,7 +51,7 @@ angular.module('ezeidApp').controller('BusinessManagerCtrl',['$scope','$rootScop
      * @param userAccess => (Integer) 0 : Hidden, 1: Read Only, 2 : Read+Create+Update, 3 : Read+Create+Update+Delete, 4 : Read+Update, 5:  Read+Update+Delete
      * @constructor
      */
-    function Module(type,title,enabled,listType,userAccess){
+    function Module(type,title,enabled,listType,userAccess,defaultFormMsg){
         if(typeof(type) == "undefined" ||
             typeof(title) == "undefined" ||
             typeof(enabled) == "undefined" ||
@@ -29,17 +59,15 @@ angular.module('ezeidApp').controller('BusinessManagerCtrl',['$scope','$rootScop
         {
             console.log("Module object creation unsuccessful ! Missing parameters");
         }
-            this.type = type,
-            this.title = (title) ? title : 'No title',
+            this.type = type;
+            this.title = (title) ? title : 'No title';
             this.enabled = (enabled) ? true : false;
             this.listType = (listType == 0 || listType == 1 || listType == 2 || listType == 3 || listType == 4) ? listType : 0;
-            this.userAccess = 0
+            this.userAccess = 0;
+            this.defaultFormMsg = defaultFormMsg;
+            this.cssClass = '';
     }
 
-
-
-
-    $scope.modules = [];
 
     $scope.activeModule = null;
     $scope.selectActive = function(moduleType){
@@ -52,83 +80,41 @@ angular.module('ezeidApp').controller('BusinessManagerCtrl',['$scope','$rootScop
      */
     $scope.loadConfig = function(){
         console.log($rootScope._userInfo);
-    };
+        var resp = [];
+        resp[0] = $rootScope._userInfo;
+        console.log(resp[0]);
+        var moduleVisibilities = resp[0].VisibleModules.split("");
+        var userModuleRights = resp[0].UserModuleRights.split("");
+        if(moduleVisibilities[0] == 1 && userModuleRights[0] > 0){
+            var salesModule = new Module(0,resp[0].SalesModuleTitle,true,resp[0].SalesItemListType,userModuleRights[0],resp[0].SalesFormMsg);
+            $scope.modules.push(salesModule);
+        }
+        if(moduleVisibilities[1] == 1 && userModuleRights[1] > 0){
+            var reservationModule = new Module(1,resp[0].AppointmentModuleTitle,true,resp[0].ReservationDisplayFormat,userModuleRights[1],resp[0].ReservationFormMsg);
+            $scope.modules.push(reservationModule);
+        }
 
-    /**
-     * Load settings from server for particular business user
-     */
-    $scope.loadSettings = function(){
-        $http({
-            url : GURL + 'ewtGetConfig',
-            method : "GET",
-            params : {
-                Token : $rootScope._userInfo.Token
-            }
-        }).success(function(resp){
-                if(resp && resp.length > 0){
+        if(moduleVisibilities[2] == 1 && userModuleRights[2] > 0){
+            var homeDeliveryModule = new Module(2,resp[0].HomeDeliveryModuleTitle,true,resp[0].HomeDeliveryItemListType,userModuleRights[2],resp[0].HomeDeliveryFormMsg);
+            $scope.modules.push(homeDeliveryModule);
+        }
 
-                    var moduleVisibilities = resp[0].VisibleModules.split("");
-                    var userAccessRights = resp[0].
-                    if(moduleVisibilities[0] === 1){
-                        var salesModule = new Module(0,resp[0].SalesTitle,true,resp[0].SalesItemListType,0);
-                    }
-                    if(moduleVisibilities[1] === 1){
-                        var salesModule = new Module(0,resp[0].ReservationTitle,true,resp[0].ReservationDisplayFormat,0);
-                    }
+        if(moduleVisibilities[3] == 1 && userModuleRights[3] > 0){
+            var serviceModule = new Module(3,resp[0].ServiceModuleTitle,true,1,userModuleRights[3],resp[0].ServiceFormMsg);
+            $scope.modules.push(serviceModule);
+        }
 
-                    if(moduleVisibilities[2] === 1){
-                        var salesModule = new Module(0,resp[0].HomeDeliveryTitle,true,resp[0].HomeDeliveryItemListType,0);
-                    }
+        if(moduleVisibilities[4] === 1 && userModuleRights[4] > 0){
+            var resumeModule = new Module(4,resp[0].CVModuleTitle,true,1,userModuleRights[4], resp[0].ResumeFormMsg);
+            $scope.modules.push(resumeModule);
+        }
 
-                    if(moduleVisibilities[3] === 1){
-                        var salesModule = new Module(0,resp[0].ServiceTitle,true,1,0);
-                    }
-
-                    if(moduleVisibilities[4] === 1){
-                        var salesModule = new Module(0,resp[0].ResumeTitle,true,1,0);
-                    }
-
-                    $scope.settings.sales.title = resp[0].SalesTitle;
-                    $scope.settings.sales.defaultFormMsg = resp[0].SalesFormMsg;
-                    $scope.settings.sales.visibility = resp[0].VisibleModules.split("")[0];
-                    $scope.settings.sales.itemListType = resp[0].SalesItemListType;
-
-
-                    $scope.settings.reservation.title = resp[0].ReservationTitle;
-                    $scope.settings.reservation.defaultFormMsg = resp[0].ReservationFormMsg;
-                    $scope.settings.reservation.visibility = resp[0].VisibleModules.split("")[1];;
-                    $scope.settings.reservation.displayFormat = resp[0].ReservationDisplayFormat;
-
-
-                    $scope.settings.homeDelivery.title = resp[0].HomeDeliveryTitle;
-                    $scope.settings.homeDelivery.defaultFormMsg = resp[0].HomeDeliveryFormMsg;
-                    $scope.settings.homeDelivery.visibility = resp[0].VisibleModules.split("")[2];;
-                    $scope.settings.homeDelivery.itemListType = resp[0].HomeDeliveryItemListType;
-
-                    $scope.settings.service.title = resp[0].ServiceTitle;
-                    $scope.settings.service.defaultFormMsg = resp[0].ServiceFormMsg;
-                    $scope.settings.service.visibility= resp[0].VisibleModules.split("")[3];;
-
-
-                    $scope.settings.resume.title = resp[0].ResumeTitle;
-                    $scope.settings.resume.defaultFormMsg = resp[0].ResumeFormMsg;
-                    $scope.settings.resume.visibility = resp[0].VisibleModules.split("")[4];;
-                    $scope.settings.resume.keywords = resp[0].ResumeKeyword;
-
-
-
-                    $scope.settings.business.dataRefreshInterval = resp[0].DataRefreshInterval;
-                    $scope.settings.business.brochureFileName = resp[0].BrochureFileName;
-                    $scope.settings.business.brochureMimeType= "";
-                    $scope.settings.business.brochureFileData = "";
-                    $scope.settings.business.keywords = "";
-                    $scope.settings.business.category = resp[0].BusinessCategoryID;
-                    $scope.settings.resume.freshersAccepted = (resp[0].FreshersAccepted === 1) ? true : false;
-                }
-                console.log(resp);
-            }).error(function(err){
-                console.log(err);
-            });
+        // Setting first module as active
+        if($scope.modules.length > 1){
+            $scope.modules[0].cssClass = 'active';
+            $scope.currentTemplate = $scope.templates[$scope.modules[0].type];
+        }
+        console.log($scope.modules);
     };
 
     $scope.loadConfig();
