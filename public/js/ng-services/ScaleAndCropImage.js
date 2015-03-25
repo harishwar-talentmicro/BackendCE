cd2195a97f60eda2e7690b5d3ef6697b2a6172f0
indrajeet <indrajeet@hirecraft.in>
2015-03-25 21:28:48 +0530

    Image Scale and Crop Service

--- public/js/ng-services/ScaleAndCropImage.js
+++ public/js/ng-services/ScaleAndCropImage.js
@@ -1,5 +1,96 @@
+/**
+ * @author Hirecraft
+ * @desc ScaleAndCrop Service
+ * @return Service Instance
+ * @example
+ * var image = $("#elem-id")[0].files[0];
+ * ScaleAndCropImage.convertToBase64(image).then(function(imageUrl){
+ *      var scaledImageUrl = ScaleAndCropImage.scalePropotional(imageUrl,90,200);
+ *      var finalImage = ScaleAndCropImage.cropImage(scaledImageUrl,90,200);
+ * });
+ *
+ *
+ */
 angular.module('ezeidApp').service('ScaleAndCropImage',['$q',function($q){
     return {
+         covertToBase64 : function (image,userType){
+
+             var deferred = $q.defer();
+
+             var fileReader = new FileReader();
+             try{
+                 fileReader.onload = function(){
+                     deferred.resolve(fileReader.result);
+                 };
+             }
+             catch(ex){
+                 deferred.reject('An error occured while reading file');
+             }
+
+             fileReader.readAsDataURL(image);
+             return deferred.promise;
+         },
+
+        scalePropotional : function(imageDataUrl,requiredHeight,requiredWidth){
+            var img = new Image();
+            img.src = imageDataUrl;
+
+            /**
+             * Calculating Differences for Height and Width to find out how
+             * the image should be scaled (based on height or based on width
+             */
+            var dHeight = img.height - requiredHeight;
+            var dWidth = img.width - requiredWidth;
+
+            /**
+             * Initially scaled image height and width are zero
+             */
+            var scaleHeight = 0;
+            var scaleWidth = 0;
+
+            if(dHeight > dWidth){
+                /**
+                 * Here Image will be scaled based on width
+                 * Calculating scale factor
+                 */
+                scaleWidth = requiredWidth;
+                scaleHeight = (img.height * scaleWidth) / img.width;
+            }
+
+            else{
+                /**
+                 * Here Image will be scaled based on height
+                 * Calculating scale factor
+                 */
+                scaleHeight = requiredHeight;
+                scaleWidth = (scaleHeight * img.width) / img.height;
+            }
+
+            var canvas = document.createElement('canvas');
+            canvas.height = scaleHeight;
+            canvas.width = scaleWidth;
+            var ctx = canvas.getContext('2d');
+            /**
+             * Scaling Image Proptionally
+             */
+            ctx.drawImage(img, 0, 0, scaleWidth,scaleHeight);
+            return canvas.toDataURL("image/jpeg", 1.0);
+        },
+
+        cropImage : function(imageDataUrl,requiredHeight,requiredWidth){
+            var img = new Image();
+            img.src = imageDataUrl;
+
+            var canvas = document.createElement('canvas');
+            canvas.height = requiredHeight;
+            canvas.width = requiredWidth;
+            var ctx = canvas.getContext('2d');
+            /**
+             * Cropping Image
+             */
+            ctx.drawImage(img,0,0,img.width,img.height,0,0,requiredHeight,requiredWidth);
+            return canvas.toDataURL("image/jpeg", 1.0);
+        }
 
     };
 }]);
