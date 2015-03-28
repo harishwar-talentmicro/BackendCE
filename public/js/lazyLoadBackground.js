71e211960487b88b8724aa47aa2027721fb5782e
indrajeet <indrajeet@hirecraft.in>
2015-03-28 20:09:08 +0530

    Footer Problem Fixed

--- public/js/lazyLoadBackground.js
+++ public/js/lazyLoadBackground.js
@@ -3,14 +3,7 @@ function lazyLoadBackground(timeout){
         var backgroundImage = new Image();
         backgroundImage.onload = function(){
             $(document).ready(function(){
-                var divHtml = '<div id="background-image-container"></div>';
-                $(divHtml).hide().appendTo("body").fadeIn(3000);
                 $("#background-image-container").css('background-image','url("/images/background.jpg")');
-
-
-                var hDifference = screen.height - $("footer").height();
-
-                $("#background-image-container").css('min-height',hDifference);
             });
         };
         backgroundImage.src = '/images/background.jpg';
