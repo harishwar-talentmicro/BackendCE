4b6adc0ffe5506cc8a0c531157745b67ad01672c
indrajeet <indrajeet@hirecraft.in>
2015-04-10 20:01:01 +0530

    Add, edit and delete functionality done

--- public/js/app/ng-controllers/profile/LocationsCtrl.js
+++ public/js/app/ng-controllers/profile/LocationsCtrl.js
@@ -47,7 +47,8 @@ angular.module('ezeidApp').controller('LocationsCtrl',[
             var found = false;
             for(var i = 0; i < this.length; i++){
                 for(var prop in this[i]){
-                    if(this[i].hasOwnProperty(key) && this[i][prop] == value){
+                    if(this[i].hasOwnProperty(key) && this[i][key] === value){
+                        console.log('prop '+prop + '  value : '+value);
                         resultIndex = i;
                         found = true;
                         break;
@@ -79,6 +80,10 @@ angular.module('ezeidApp').controller('LocationsCtrl',[
          */
         var mapList = {};
 
+        /**
+         * List of states loaded in Editing mode based on country ID
+         * @type {Array}
+         */
         $scope.editStateList = [];
 
 
@@ -121,6 +126,7 @@ angular.module('ezeidApp').controller('LocationsCtrl',[
         $scope.initializeMap = function(index){
             if(!$scope.locationsToggleIndex[index].isMapInitialized)
             {
+                console.log('Line 125 map initialization in progress');
                 mapList['map'+index] = new GoogleMaps();
                 $scope.locationsToggleIndex[index].isMapInitialized = true;
 
@@ -151,7 +157,8 @@ angular.module('ezeidApp').controller('LocationsCtrl',[
                     containerElement = 'map-location-'+index;
                 }
 
-
+                console.log('container-element');
+                console.log(containerElement);
                 mapList['map'+index].createMap(containerElement,$scope,'findCurrentLocation('+index+')');
                 mapList['map'+index].renderMap();
                 mapList['map'+index].mapIdleListener().then(function(){
@@ -166,6 +173,7 @@ angular.module('ezeidApp').controller('LocationsCtrl',[
                 });
             }
             else{
+                console.log('Line 171 no map initialization');
                 mapList['map'+index].clearAllMarkers();
                 if(index == 0){
                     /**
@@ -267,16 +275,20 @@ angular.module('ezeidApp').controller('LocationsCtrl',[
         };
 
 
+        /**
+         * Details of Location currently in editing mode
+         * @type {{LocTitle: string, LocID: number, AddressLine1: string, AddressLine2: string, CityID: string, StateID: string, CityTitle: string, StateTitle: string, CountryID: number, CountryName: string, ISDPhoneNumber: string, PhoneNumber: string, ISDMobileNumber: string, MobileNumber: string, ParkingStatus: string, Website: string, Picture: string, PictureFileName: string, PostalCode: string, Latitude: number, Longitude: number, Altitude: number}}
+         */
         $scope.editLocationDetails = {
-            LocTitle : '',
-            LocID : '',
+            LocTitle : 'New Location',
+            LocID : 0,
             AddressLine1 : '',
             AddressLine2 : '',
             CityID : '',
             StateID : '',
             CityTitle : '',
             StateTitle : '',
-            CountryID : '',
+            CountryID : 98,
             CountryName : '',
             ISDPhoneNumber : '',
             PhoneNumber: '',
@@ -341,6 +353,12 @@ angular.module('ezeidApp').controller('LocationsCtrl',[
         };
 
 
+        $scope.saveLocation = function(index){
+            /**
+             * @todo Send a API Call to save the location currently in edit mode
+             */
+        };
+
         /**
          * To be called when stateList for current editing location is loaded successfully
          * and models are assigned
@@ -360,26 +378,56 @@ angular.module('ezeidApp').controller('LocationsCtrl',[
             mapList['map'+index].placeMarker(marker,$scope.setEditCurrentLocation);
         };
 
+
         /**
-         * @todo saveLocation();
+         * Add a new secondary location locally
+         * to be saved on server
          */
-
-        $scope.deleteSecondaryLocation = function(){
-            /**
-             * @todo Deleting secondaryLocation
-             */
-        };
-
         $scope.addSecondaryLocation = function(){
+            console.log('I am clicked');
             var locIndex = $scope.locationsToggleIndex.indexOfWhere('savedOnServer',false);
+
             if(locIndex !== -1){
                 $scope.toggleAllLocations(locIndex);
                 $timeout(function(){
                     $scope.toggleMapControls(locIndex);
                     $scope.editLocation(locIndex);
                 },4000);
+                return;
             }
             var newLoc = angular.copy($scope.editLocationDetails);
+            var initialLength = $scope.locationsToggleIndex.length; //3
+
+            $scope.locationsToggleIndex.length += 1; //4 now prev 3
+            $scope.secondaryLocations.length += 1;  //3 now prev 2
+
+            $scope.locationsToggleIndex[initialLength] = {
+                viewMode : false,
+                editMode : false,
+                isMapInitialized : false,
+                savedOnServer: false
+            };
+            $scope.secondaryLocations[initialLength-1] =  newLoc;
+
+            $scope.locationsToggleIndex[initialLength].viewMode = true;
+            $scope.locationsToggleIndex[initialLength].editMode = true;
+
+            $timeout(function(){
+                $scope.initializeMap(initialLength);
+                $timeout(function(){
+                    $scope.editLocation(initialLength);
+                    $scope.toggleMapControls(initialLength);
+                },2000);
+            },1000);
+        };
+
+
+        /**
+         * Deletes a secondary Location
+         * @param locIndex
+         */
+        $scope.deleteSecondaryLocation = function(locIndex){
+
         };
 
     }]);
