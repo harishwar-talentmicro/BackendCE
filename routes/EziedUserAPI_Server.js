f2799977963a995b0a883ed40870374b7225cd86
gowrishankar <gowrishankar@hirecraft.in>
2015-04-11 14:27:30 +0530

    remove EmailID parameter in AddLocation method.

--- routes/EziedUserAPI_Server.js
+++ routes/EziedUserAPI_Server.js
@@ -2224,22 +2224,16 @@ exports.FnAddLocation = function (req, res) {
         var PIN = req.body.PIN;
         var PhoneNumber = req.body.PhoneNumber;
         var MobileNumber = req.body.MobileNumber;
-        var EMailID = req.body.EMailID;
-
         var Picture = req.body.Picture;
         var PictureFileName = req.body.PictureFileName;
         var Website = req.body.Website;
         var ISDPhoneNumber = req.body.ISDPhoneNumber;
         var ISDMobileNumber = req.body.ISDMobileNumber;
-
-        //below line of code is commented for phase 1
-        
+        //below line of code is commented for phase 1        
         var ParkingStatus = parseInt(req.body.ParkingStatus);
-
         if (ParkingStatus.toString() == 'NaN') {
             ParkingStatus = 0;
         }
-
         if (PIN == '') {
             PIN = null;
         }
@@ -2252,9 +2246,8 @@ exports.FnAddLocation = function (req, res) {
                         var InsertQuery = db.escape(TID) + ',' + db.escape(Token) + ',' + db.escape(LocTitle) + ',' + db.escape(Latitude) 
                         + ',' + db.escape(Longitude) + ',' + db.escape(Altitude) + ',' + db.escape(AddressLine1) + ',' + db.escape(AddressLine2) 
                         + ',' + db.escape(CityName) + ',' + db.escape(StateID) + ',' + db.escape(CountryID) + ',' + db.escape(PostalCode) 
-                        + ',' + db.escape(PIN) + ',' + db.escape(PhoneNumber) + ',' + db.escape(MobileNumber) + ',' + db.escape(EMailID) 
-                         + ',' + db.escape(Picture) + ',' + db.escape(PictureFileName) + ',' + db.escape(Website)  
-                        + ',' +   db.escape(ISDPhoneNumber) + ',' + db.escape(ISDMobileNumber) + ',' + db.escape(ParkingStatus);
+                        + ',' + db.escape(PIN) + ',' + db.escape(PhoneNumber) + ',' + db.escape(MobileNumber)  + ',' + db.escape(Picture)
+                        + ',' + db.escape(PictureFileName) + ',' + db.escape(Website) + ',' +   db.escape(ISDPhoneNumber) + ',' + db.escape(ISDMobileNumber) + ',' + db.escape(ParkingStatus);
 
                         db.query('CALL pInsertLocationData(' + InsertQuery + ')', function (err, InsertResult) {
                             if (!err) {
@@ -2264,7 +2257,7 @@ exports.FnAddLocation = function (req, res) {
                                         // console.log(InsertResult);
                                         console.log('Addlocation: Location added successfully');
 
-                                        var selectqry = 'Select tlocations.TID,MasterID,EZEID,LocTitle,Latitude,Longitude,Altitude,AddressLine1,AddressLine2,StateID,CountryID,PostalCode,PIN,EMailID,EMailVerifiedID,ifnull(PhoneNumber,"") as PhoneNumber,MobileNumber,ifnull(ISDPhoneNumber,"") as ISDPhoneNumber ,ifnull(ISDMobileNumber,"") as ISDMobileNumber,CreatedDate,LUDate,Website,SeqNo,Picture,PictureFileName,ifnull((Select CityName from mcity where CityID=tlocations.CityID),"") as CityTitle,ifnull(ParkingStatus,0) as ParkingStatus from tlocations';
+                                        var selectqry = 'Select tlocations.TID,MasterID,EZEID,LocTitle,Latitude,Longitude,Altitude,AddressLine1,AddressLine2,StateID,CountryID,PostalCode,PIN,EMailVerifiedID,ifnull(PhoneNumber,"") as PhoneNumber,MobileNumber,ifnull(ISDPhoneNumber,"") as ISDPhoneNumber ,ifnull(ISDMobileNumber,"") as ISDMobileNumber,CreatedDate,LUDate,Website,SeqNo,Picture,PictureFileName,ifnull((Select CityName from mcity where CityID=tlocations.CityID),"") as CityTitle,ifnull(ParkingStatus,0) as ParkingStatus from tlocations';
 
                                         if (TID == 0) {
                                             selectqry = selectqry + ' order by tlocations.TID desc limit 1';
@@ -2354,6 +2347,7 @@ exports.FnAddLocation = function (req, res) {
         throw new Error(ex);
     }
 };
+
 
 //method for delete location
 exports.FnDeleteLocation = function (req, res) {
