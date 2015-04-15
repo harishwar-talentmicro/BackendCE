43923e1136db2feca4c4889dfb07224fe10b97e6
indrajeet <indrajeet@hirecraft.in>
2015-04-15 12:16:25 +0530

    Migrating HTML Files to Profile Manager > Configuration

--- public/js/app/ng-controllers/profile/configuration/ConfigurationNavigationCtrl.js
+++ public/js/app/ng-controllers/profile/configuration/ConfigurationNavigationCtrl.js
@@ -1,7 +1,21 @@
 /**
  * Created by admin on 6/3/15.
  */
-angular.module('ezeidApp').controller('ConfigurationNavigationCtrl',['$scope','$rootScope','$http','Notification','GURL',function($scope,$rootScope,$http,Notification,GURL){
+angular.module('ezeidApp').controller('ConfigurationNavigationCtrl',[
+    '$scope',
+    '$rootScope',
+    '$http',
+    'Notification',
+    'GURL',
+    '$location',
+    function(
+        $scope,
+        $rootScope,
+        $http,
+        Notification,
+        GURL,
+        $location
+        ){
 
     /**
      * Hiding progress loader when userDetails are loaded successfully
@@ -11,22 +25,27 @@ angular.module('ezeidApp').controller('ConfigurationNavigationCtrl',['$scope','$
             $scope.dataProgressLoader.dataLoadInProgress = false;
             $scope.dataProgressLoader.dataLoadError = false;
             $scope.dataProgressLoader.dataLoadComplete = true;
+
+            if(newVal.IDTypeID !== 2 && newVal.SelectionType !== 2){
+                $location.path('/profile-manager/profile');
+            }
         }
     });
 
     /**
      * @todo Create and add template for resource creation and mapping with reservation items
      */
-    $scope.pageSubuser = "html/subusers.html";
-    $scope.pageItemMaster = "html/item-master.html";
-    $scope.pageResources = "html/reservation-resource.html";
-    $scope.pageStatusTypes = "html/status-types.html";
-    $scope.pageActionTypes = "html/action-types.html";
-    $scope.pageRules = "html/rules.html";
-    $scope.pageWorkingHours = "html/working-hours.html";
-    $scope.pageHolidayCalender = "html/holiday-calender.html";
-    $scope.pageBusinessListing = "html/business-listing.html";
-    $scope.pageModuleSettings = "html/module-settings.html";
+
+        $scope.pageSubuser = "html/profile/configuration/subviews/subusers.html";
+        $scope.pageItemMaster = "html/profile/configuration/subviews/item-master.html";
+        $scope.pageResources = "html/profile/configuration/subviews/reservation-resource.html";
+        $scope.pageStatusTypes = "html/profile/configuration/subviews/status-types.html";
+        $scope.pageActionTypes = "html/profile/configuration/subviews/action-types.html";
+        $scope.pageRules = "html/profile/configuration/subviews/rules.html";
+        $scope.pageWorkingHours = "html/profile/configuration/subviews/working-hours.html";
+        $scope.pageHolidayCalender = "html/profile/configuration/subviews/holiday-calender.html";
+        $scope.pageBusinessListing = "html/profile/configuration/subviews/business-listing.html";
+        $scope.pageModuleSettings = "html/profile/configuration/subviews/module-settings.html";
 
     $scope.activeSubTemplate = $scope.pageModuleSettings;
 
