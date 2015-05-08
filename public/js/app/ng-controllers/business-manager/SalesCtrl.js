d0fff30be5940151d4fb41c0a7dc5f42a116d33c
indrajeet <indrajeet@hirecraft.in>
2015-05-08 12:34:22 +0530

    Adding inline editing functionality to sales manager

--- public/js/app/ng-controllers/business-manager/SalesCtrl.js
+++ public/js/app/ng-controllers/business-manager/SalesCtrl.js
@@ -43,6 +43,8 @@
             };
 
 
+            $scope.$emit('$preLoaderStart');
+
 
             $(document).on('click','.popover-close',function(){
                 $('*[data-toggle="popover"]').popover('hide');
@@ -52,6 +54,8 @@
             $scope.statusType = null;
             $scope.txList = [];
             $scope.totalPages = 1;
+            $scope.txStatusTypes = [];
+            $scope.txActionTypes = [];
 
             $scope.showModal = false;
             $scope.modalBox = {
@@ -85,6 +89,30 @@
                   durationScale : 0,
                   itemList : []
               }
+            };
+
+            $scope.editModes = [];
+
+
+            /**
+             * Toggles the edit mode for particular transaction
+             * Inline editing
+             * @param index
+             */
+            $scope.toggleAllEditMode = function(index){
+                console.log(index);
+                if(typeof(index) === "undefined")
+                {
+                    index = -1;
+                }
+                for(var c = 0; c < $scope.editModes.length; c++){
+                    if(c === index){
+                        $scope.editModes[c] = true;
+                    }
+                    else{
+                        $scope.editModes[c] = false;
+                    }
+                }
             };
 
             /**
@@ -136,7 +164,12 @@
             };
 
 
-
+            /**
+             * Loads all transactions
+             * @param pageNo
+             * @param statusType
+             * @returns {*}
+             */
             $scope.loadTransaction = function(pageNo,statusType){
                 var defer = $q.defer();
                 if(!pageNo){
@@ -165,6 +198,10 @@
                         $scope.totalPage = resp.TotalPage;
                         $scope.pageNumber = pageNo;
                         $scope.txList = resp.Result;
+
+                        for(var a = 0; a < resp.Result.length; a++){
+                            $scope.editModes.push(false);
+                        }
                     }
                     else{
 
@@ -177,7 +214,75 @@
                 return defer.promise;
             };
 
-            console.log($scope.modules);
+
+            /**
+             * Load Transaction Action Types
+             * @returns {*}
+             */
+            $scope.loadTxActionTypes = function(){
+                var defer = $q.defer();
+                $http({
+                    url : GURL + 'ewtGetActionType',
+                    method : 'GET',
+                    params : {
+                        Token : $rootScope._userInfo.Token,
+                        FunctionType : 0    // For Sales
+                    }
+                }).success(function(resp){
+                    if(resp && resp !== 'null' && resp.length > 0){
+                        /**
+                         * Change
+                         * 1. $scope.totalPages
+                         * 2. $scope.pageNumber
+                         * 3. $scope.txList
+                         */
+                        $scope.txActionTypes = resp;
+                    }
+                    else{
+
+                        $scope.txActionTypes = [];
+                    }
+                    defer.resolve(resp);
+                }).error(function(err){
+                    defer.reject();
+                });
+                return defer.promise;
+            };
+
+
+            /**
+             * Load Transaction Status Types
+             * @returns {*}
+             */
+            $scope.loadTxStatusTypes = function(){
+                var defer = $q.defer();
+                $http({
+                    url : GURL + 'ewtGetStatusType',
+                    method : 'GET',
+                    params : {
+                        Token : $rootScope._userInfo.Token,
+                        FunctionType : 0    // For Sales
+                    }
+                }).success(function(resp){
+                    if(resp && resp !== 'null' && resp.length > 0){
+                        /**
+                         * Change
+                         * 1. $scope.totalPages
+                         * 2. $scope.pageNumber
+                         * 3. $scope.txList
+                         */
+                        $scope.txStatusTypes = resp;
+                    }
+                    else{
+
+                        $scope.txStatusTypes = [];
+                    }
+                    defer.resolve(resp);
+                }).error(function(err){
+                    defer.reject();
+                });
+                return defer.promise;
+            };
 
 
             $scope.$watch('pageNumber',function(newVal,oldVal){
@@ -191,12 +296,23 @@
                     });
                 }
             });
-            $scope.$emit('$preLoaderStop');
-            $scope.loadTransaction().then(function(){
-                $scope.$emit('$preLoaderStart');
+
+
+            $scope.loadTxActionTypes().then(function(){
+                $scope.loadTxStatusTypes().then(function(){
+                    $scope.loadTransaction().then(function(){
+                        $scope.$emit('$preLoaderStop');
+                    },function(){
+                        $scope.$emit('$preLoaderStop');
+                    });
+                },function(){
+                    $scope.$emit('$preLoaderStop');
+                });
             },function(){
                 $scope.$emit('$preLoaderStop');
             });
+
+
 
         }]);
 
