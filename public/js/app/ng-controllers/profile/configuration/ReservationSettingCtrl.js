bcd11c9f52567a03d3e7da7de4a68f1ac3c86bf3
unknown <krunal.patel@hirecraft.in>
2015-05-27 20:35:02 +0530

    commit final changes

--- public/js/app/ng-controllers/profile/configuration/ReservationSettingCtrl.js
+++ public/js/app/ng-controllers/profile/configuration/ReservationSettingCtrl.js
@@ -1,66 +1,44 @@
 /**
- * StatusTypesCtrl
- * RuleFilter can be used for Filtering Status too, as both contain FunctionType(Sales,Reservation,HomeDelivery..) Property
+ * ReservationSettingCtrl
+ *
  */
-angular.module('ezeidApp').controller('ReservationSettingCtrl',['$scope','$rootScope','$http','Notification','$filter','MsgDelay','GURL',function($scope,$rootScope,$http,Notification,$filter,MsgDelay,GURL){
-    var functionTypes = ['sales','reservation','homeDelivery','service','resume'];
+angular.module('ezeidApp').controller('ReservationSettingCtrl',['$scope',
+    '$rootScope',
+    '$http',
+    'Notification',
+    '$filter',
+    'MsgDelay',
+    'GURL',function(
+        $scope,
+        $rootScope,
+        $http,
+        Notification,
+        $filter,
+        MsgDelay,
+        GURL){
 
     //Initially First Tab is selected
     $scope.selectedTab = 1;
 
-    console.log("test");
-    /**
-     * Main EZEID logged in as business user and having verified status
-     * @type {null}
-     */
-    $scope.masterUser = null;
-
-    /**
-     * Sub user Status Mapping
-     * @type {{1: string, 2: string}}
-     */
-    $scope.status = {
-        1 : "Active",
-        2 : "Inactive"
-    };
-
-
-    /***
-     * All types of items will reside in this model
-     * @type {{sales: Array, reservation: Array, homeDelivery: Array, service: Array, resume: Array}}
-     */
-    $scope.txStatuses = {
-        sales : [],
-        reservation : [],
-        homeDelivery : [],
-        service : [],
-        resume : []
-    };
-
-
-
-    // Item types (functionTypes) available to the system
-    $scope.itemTypes = [
-        'Sales',
-        'Reservation',
-        'Home Delivery',
-        'Service',
-        'Resume'
-    ];
-
     $scope.modalBox = {
-        title : "Add new Status Type",
-        txStatus : {
+        item : {
             TID : 0,
-            type : 0,           // Function Type
             title : "",
-            progress : 0,       // Progress % for this title
-            status : 1,         // 1 : Active
-            notificationMsg : "",   // Notification Message
-            notificationMailMsg : "",   // Notification Mail Message
-            statusValue : 0
+            rate : 0,
+            status : 1,
+            description : "",
+            picture : "",
+            duration : null
         }
     };
+
+    getUserDetails();
+
+    /**
+     * Resource Status Mapping
+     * @type {{1: string, 2: string}}
+     */
+    $scope.status = 1 ;
 
     /**
      *     Open Modal box for user
@@ -68,58 +46,76 @@ angular.module('ezeidApp').controller('ReservationSettingCtrl',['$scope','$rootS
     $scope.showModal = false;
     $scope.toggleModalBox = function(type,index){
 
-        if(typeof(index) !== "undefined" && typeof(type) !== "undefined"){
-            // ////console.log(index+'   '+type);
-            $scope.modalBox.txStatus = angular.copy($scope.txStatuses[functionTypes[type]][index]);
-            $scope.modalBox.title = "Update Status Type";
-        }
-        else{
+        loadSubuserList();
 
-            $scope.resetModalData();
-            $scope.modalBox = {
-                title : "Add new Status Type",
-                txStatus : {
-                    TID : 0,
-                    type : type,           // Function Type
-                    title : "",
-                    progress : 0,       // Progress % for this title
-                    status : 1,         // 1 : Active
-                    notificationMsg : "",   // Notification Message
-                    notificationMailMsg : "",   // Notification Mail Message
-                    statusValue : 0
-                }
-            };
-        }
+      /*  if(typeof(index) !== "undefined" && typeof(type) !== "undefined"){
+         // ////console.log(index+'   '+type);
+         $scope.modalBox.txStatus = angular.copy($scope.txStatuses[functionTypes[type]][index]);
+         $scope.modalBox.title = "Update Status Type";
+         }
+         else{
+
+         $scope.resetModalData();
+         $scope.modalBox = {
+         title : "Add new Status Type",
+         txStatus : {
+         TID : 0,
+         type : type,           // Function Type
+         title : "",
+         progress : 0,       // Progress % for this title
+         status : 1,         // 1 : Active
+         notificationMsg : "",   // Notification Message
+         notificationMailMsg : "",   // Notification Mail Message
+         statusValue : 0
+         }
+         };
+         }*/
         $scope.showModal = !$scope.showModal;
     };
 
-    $scope.resetModalData = function(){
-        $scope.modalBox = {
-            title : "Add new Status Type",
-            txStatus : {
-                TID : 0,
-                type : 0,           // Function Type
-                title : "",
-                progress : 0,       // Progress % for this title
-                status : 1,         // 1 : Active
-                notificationMsg : "",   // Notification Message
-                notificationMailMsg : "",   // Notification Mail Message
-                statusValue : 0
-            }
-        };
+    /**
+     *     Open Modal box for user
+     */
+    $scope.showReservationCerviceModal = false;
+    $scope.toggleReservationServiceModalBox = function(type,index){
+
+        $scope.showReservationCerviceModal = !$scope.showReservationCerviceModal;
     };
 
     /**
-     * Function type count for loading rules based on functionType
-     * @type {number}
+     * Loads list of subusers
      */
-    $scope.functionTypeCount = 0;
+    function loadSubuserList()
+    {
+       $scope.$emit('$preLoaderStart');
+       $scope.subusers = [];
+       $http({
+            url : GURL + 'ewtGetSubUserList',
+            method : "GET",
+            params : {
+                Token : $rootScope._userInfo.Token,
+                MasterID : $scope.masterUser.MasterID
+            }
+        }).success(function(resp){
+                if(resp && resp.length > 0 && resp !== 'null')
+                {
+                    $scope.subusers = resp;
+                }
+                else{
+                    //Notification.error({ message: "No subusers added ", delay : MsgDelay});
+                }
+                $scope.$emit('$preLoaderStop');
+            }).error(function(err){
+                $scope.$emit('$preLoaderStop');
+              //  Notification.error({ message: "No subusers added ", delay : MsgDelay});
+            });
+    };
+
     /**
-     * Getting Master User Details
-     * Then calling loadRules Function to load all statuses
+     * Getting master user details
      */
-    $scope.getMasterUserDetails = function(){
-        $http({
+    function getUserDetails(){
+       $http({
             url : GURL + 'ewtGetUserDetails',
             method : "GET",
             params :{
@@ -128,164 +124,96 @@ angular.module('ezeidApp').controller('ReservationSettingCtrl',['$scope','$rootS
         }).success(function(resp){
                 if(resp.length>0){
                     $scope.masterUser = resp[0];
-                    $scope.loadTxStatusTypes();
                 }
             }).error(function(err){
                 Notification.error({ message: "Something went wrong! Check your connection", delay: MsgDelay });
             });
-
     };
-
-    $scope.statusValueLimit = 10;
 
     /**
-     * For generating Status Values from 1 to 40
-     * @param num
-     * @returns {Array}
+     * Validates the data while adding items
+     * @param item
+     * @returns {boolean}
      */
-    $scope.getNumber = function(num) {
-        return new Array(num);
-    }
+    function validateItem(){
+        console.log("sai77");
 
 
-    $scope.validateTxStatus = function(txStatus){
-        /**
-         * @todo Validate all fields and then allow saving of txStatus
-         */
-        return true;
+        console.log($scope.modalBox);
+        console.log($scope.mResourceStatus);
+        console.log($scope.mOperatorID);
+        console.log($scope.mResourceDescription);
+
+       /* var err = [];
+        if(item.title.length < 1){
+            err.push('Item Title is empty');
+        }
+        if(item.description.length < 1 ){
+            err.push('Add description to this item');
+        }
+        if($scope.globalConfig.itemListType[item.type] > 1){
+            if(!item.picture){
+                err.push('Please select a picture for this item');
+            }
+            if($scope.globalConfig.itemListType[item.type] > 2){
+                if(!item.rate){
+                    err.push('Please enter rate of item');
+                }
+            }
+        }
+
+        if(err.length > 0){
+            for(var i = 0; i < err.length; i++){
+                Notification.error({ message : err[i], delay : 5000});
+            }
+            return false;
+        }
+        return true;*/
     };
 
-    $scope.saveTxStatus = function(){
-        if($scope.validateTxStatus($scope.modalBox.txStatus)){
-            var data = {
-                Token : $rootScope._userInfo.Token,
-                TID : $scope.modalBox.txStatus.TID,
-                StatusTitle : $scope.modalBox.txStatus.title,
-                MasterID : $scope.masterUser.MasterID,
-                FunctionType : $scope.modalBox.txStatus.type,
-                ProgressPercent : $scope.modalBox.txStatus.progress,
-                Status : $scope.modalBox.txStatus.status,
-                NotificationMsg : $scope.modalBox.txStatus.notificationMsg,
-                NotificationMailMsg: $scope.modalBox.txStatus.notificationMailMsg,
-                StatusValue : $scope.modalBox.txStatus.statusValue
-            };
+    $scope.saveResource = function(){
+       /* var data = {
+            Token : $rootScope._userInfo.Token,
+            MasterID : $scope.masterUser.MasterID,
 
-            $http({
-                url : GURL + 'ewmSaveStatusType',
-                data : data,
-                method : 'POST'
+            TID : $scope.modalBox.item.TID,
+            FunctionType : $scope.modalBox.item.type,
+            ItemName : $scope.modalBox.item.title,
+            ItemDescription : $scope.modalBox.item.description,
+            Pic : ($scope.modalBox.item.picture == $scope.defaultPicture) ? null : $scope.modalBox.item.picture,
+            Rate : ($scope.modalBox.item.rate)? $scope.modalBox.item.rate : 0.00,
+            Status : $scope.modalBox.item.status,
+            ItemDuration : $scope.modalBox.item.duration
+
+        };*/
+
+        /**
+         * Validates Items and then save it to server
+         */
+        if(validateItem()){
+            /*$http({
+                url : GURL + 'ewtSaveItem',
+                method : "POST",
+                data : data
             }).success(function(resp){
-                if(resp && resp !== 'null' && resp.hasOwnProperty('IsSuccessfull')){
-                    if(resp.IsSuccessfull){
-                        Notification.success({ message : 'Status added successfully', delay : MsgDelay});
-                        if($scope.modalBox.txStatus.TID)
-                        {
-                            var salesArr = $scope.txStatuses[functionTypes[$scope.modalBox.txStatus.type]];
-                            var index  = salesArr.indexOfWhere('TID',$scope.modalBox.txStatus.TID);
-                            for(var prop in salesArr[index]){
-                                if(salesArr[index].hasOwnProperty(prop)){
-                                    // ////console.log(prop);
-                                    $scope.txStatuses[functionTypes[$scope.modalBox.txStatus.type]][index][prop] = $scope.modalBox.txStatus[prop];
-                                }
-                            }
-
+                    if(resp && resp.hasOwnProperty("IsSuccessfull")){
+                        if(resp.IsSuccessfull){
+                            $scope.loadItems($scope.modalBox.item.type);
+                            $scope.toggleModalBox();
+                            Notification.success({ message : 'Item added successfully', delay : 5000 });
                         }
                         else{
-                            var txStatus = angular.copy($scope.modalBox.txStatus);
-                            $scope.txStatuses[functionTypes[$scope.modalBox.txStatus.type]].push(txStatus);
+                            Notification.error({ message : 'An error occured while saving item! Please try again', delay : 5000});
                         }
-
-                        $scope.resetModalData();
-                        $scope.toggleModalBox();
                     }
                     else{
-                        Notification.error({ message : 'An error occured while adding status', delay : MsgDelay});
+                        Notification.error({ message : 'An error occured while saving item! Please try again', delay : 5000});
                     }
-                }
-                else{
-                    Notification.error({ message : 'An error occured while adding status', delay : MsgDelay});
-                }
-            }).error(function(err){
-                    Notification.error({ message : 'An error occured while adding status', delay : MsgDelay});
-            });
+                }).error(function(err){
+                    Notification.error({ message : 'An error occured while saving item! Please try again', delay : 2000});
+                });*/
         }
-        else{
-            return;
-        }
+
     };
-
-
-    $scope.loadTxStatusTypes = function(functionType){
-
-        var fType = (typeof(functionType) !== "undefined") ? functionType : $scope.functionTypeCount;
-        $http({
-            url : GURL + 'ewtGetStatusType',
-            method : "GET",
-            params : {
-                Token : $rootScope._userInfo.Token,
-                FunctionType : fType,
-                MasterID : ($scope._userInfo.MasterID) ? $scope._userInfo.MasterID : $scope.masterUser.MasterID
-            }
-        }).success(function(resp){
-
-
-                /**
-                 * If functionType is set in function, it will be called once only
-                 */
-
-                if(typeof(functionType) !== "undefined"){
-                    if(resp && resp.length > 0 && resp !== 'null'){
-                        for(var i = 0 ; i < resp.length ; i++){
-                            var txStatus = {
-                                TID : resp[i].TID,
-                                type : fType,           // Function Type
-                                title : resp[i].StatusTitle,
-                                progress : (resp[i].ProgressPercent) ? resp[i].ProgressPercent : 0,       // Progress % for this title
-                                status : resp[i].Status,         // 1 : Active
-                                notificationMsg : resp[i].NotificationMsg,   // Notification Message
-                                notificationMailMsg : resp[i].NotificationMailMsg,   // Notification Mail Message
-                                statusValue : (resp[i].StatusValue) ? resp[i].StatusValue : 0
-                            }
-                            $scope.txStatuses[functionTypes[fType]].push(txStatus);
-
-                        }
-
-                        // ////console.log($scope.txStatuses);
-                    }
-                }
-
-                /**
-                 * It will try to load all items and will increase the counter( loading all types of items initially)
-                 */
-                else{
-                    if(resp && resp.length > 0 && resp !== 'null'){
-                        for(var i = 0 ; i < resp.length ; i++){
-                            var txStatus = {
-                                TID : resp[i].TID,
-                                type : fType,           // Function Type
-                                title : resp[i].StatusTitle,
-                                progress : (resp[i].ProgressPercent) ? resp[i].ProgressPercent : 0,       // Progress % for this title
-                                status : resp[i].Status,         // 1 : Active
-                                notificationMsg : resp[i].NotificationMsg,   // Notification Message
-                                notificationMailMsg : resp[i].NotificationMailMsg,   // Notification Mail Message
-                                statusValue : (resp[i].StatusValue) ? resp[i].StatusValue : 0
-                            }
-                            $scope.txStatuses[functionTypes[fType]].push(txStatus);
-                        }
-                        // ////console.log($scope.txStatuses);
-                    }
-
-                    $scope.count += 1;
-                    if($scope.count < 5){
-                        $scope.loadItems();
-                    }
-                }
-            }).error(function(err){
-                // ////console.log(err);
-            });
-    };
-
-
-    $scope.getMasterUserDetails();
 
 }]);
