37e809474f3ce8b26f60f03956782220afd171fc
indrajeet <indrajeet@hirecraft.in>
2015-06-15 15:30:21 +0530

    Adding search for items to sales module Adding filter file to index.html

---
+++ public/js/app/ng-filters/business-item-search.js
@@ -0,0 +1,21 @@
+(function(){
+    angular.module('ezeidApp').filter('businessItemSearch',function(){
+        return function(itemList,searchTerm){
+            if(!itemList){
+                return [];
+            }
+            if(!searchTerm){
+                return itemList;
+            }
+
+            var returnList = [];
+            var regex = new RegExp(searchTerm);
+            for(var i=0; i< itemList.length; i++){
+                if(regex.test(itemList[i].ItemName)){
+                    returnList.push(itemList[i]);
+                }
+            }
+            return returnList;
+        };
+    });
+})();
