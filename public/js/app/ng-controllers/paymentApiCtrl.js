6345b9a3c169f4d411ef0618b38bb1ccf91e556e
sandeep_gantait <sandeep_gantait>
2015-06-15 19:40:10 +0530

    Payment API Html completed

--- public/js/app/ng-controllers/paymentApiCtrl.js
+++ public/js/app/ng-controllers/paymentApiCtrl.js
@@ -1,3 +1,49 @@
 /**
- * 
+ * Handles the payment API integration
+ *
+ * @author Sandeep Gantait
+ * @since 20150615
  */
+
+var res = angular.module('ezeidApp').
+    controller('paymentApiCtrl', [
+        '$rootScope',
+        '$scope',
+        '$http',
+        '$q',
+        '$timeout',
+        'Notification',
+        '$filter',
+        '$window',
+        'GURL',
+        '$interval',
+        'MsgDelay',
+        '$location',
+        '$routeParams',
+        'GoogleMaps',
+        '$route',
+        'UtilityService',
+        function (
+            $rootScope,
+            $scope,
+            $http,
+            $q,
+            $timeout,
+            Notification,
+            $filter,
+            $window,
+            GURL,
+            $interval,
+            MsgDelay,
+            $location,
+            $routeParams,
+            GoogleMap,
+            $route,
+            UtilityService
+        ) {
+
+            /**
+             * All Mega-Code goes here
+             */
+        }
+    ]);
