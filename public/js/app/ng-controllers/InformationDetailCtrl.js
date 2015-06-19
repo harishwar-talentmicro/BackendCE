f89a728f8cf894edadc4c02d7700a74dd2b6e72a
sandeep_gantait <sandeep_gantait>
2015-06-19 19:25:35 +0530

    changing parameter name to ezeone from ezeid ($routeParams)

--- public/js/app/ng-controllers/InformationDetailCtrl.js
+++ public/js/app/ng-controllers/InformationDetailCtrl.js
@@ -443,7 +443,7 @@ angular.module('ezeidApp').
                 }
                 else
                 {
-                    var params = '?ezeid='+_Ezeid;
+                    var params = '?ezeone='+_Ezeid;
                     $timeout(function(){
                         $location.url('/service-reservation'+params+'&name='+$scope.SearchInfo.CompanyName);
                     },500);
