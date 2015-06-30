0f55cd5c1b511762d30221d38fb0b1bfb40a9793
sandeep_gantait <sandeep_gantait>
2015-06-30 15:02:23 +0530

    changes in search result module buttons and links

--- public/js/app/ng-controllers/SearchResultCtrl.js
+++ public/js/app/ng-controllers/SearchResultCtrl.js
@@ -278,7 +278,8 @@ var res = angular.module('ezeidApp').
                     $scope.isResultNumber = (result && result !== 'null') ? ((result.length > 0) ? 1 : 0) : 0;
 
                     $scope.searchListData = (result && result !== 'null') ? ((result.length > 0) ? result : []) : [];
-
+                    console.log('--------------');
+                    console.log($scope.searchListData);
                     if ( data && data != 'null' && data.length>0)
                     {
                         $scope.SearchResultCount = data.length;
@@ -1042,5 +1043,46 @@ var res = angular.module('ezeidApp').
                     $scope.paginationPrevious = false;
                 }
             }
+
+            /**
+             * refine the module visibility: if there are more then 3 modules are visible show only 3
+             * @param val: the original module visibility eg: 11111 or 11010
+             * @returns {string} 11100 or 10011 : the number of 1s can't be more than 3
+             */
+            $scope.refineModuleVisibility = function(val)
+            {
+                if(val != null) {
+                    val = val.split("");
+                    var count = 0;
+                    for(var i=0; i<val.length; i++)
+                    {
+                        if(val[i] == 1 && count < 3)
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            val[i] = 0;
+                        }
+                    }
+                    val = val.join(",");
+                    return val.replace(/,/g,'');
+                }
+            }
+
+            /**
+             *
+             */
+            $scope.redirectUrl = function(url){
+                if(!$rootScope._userInfo.Token || $rootScope._userInfo.Token == 2){
+                    angular.element('#SignIn_popup').slideUp();
+                    $rootScope.loginPromise = $q.defer();
+                    $rootScope.loginPromise.promise.then(function(){
+                        $location.url(url);
+                    });
+                    return;
+                }
+                $location.url(url);
+            };
         }
     ]);
