6a22b043b9e07437b1f05017288dd2024b5e7096
indrajeet <indrajeet@hirecraft.in>
2015-07-30 11:58:19 +0530

    Resume Jobs and Job Seeker search view separated according to module structure

---
+++ public/js/app/ng-controllers/business-manager/resume/ResumeJobSeekerCtrl.js
@@ -0,0 +1,46 @@
+/**
+ * Functionality for searching job seekers(users who are interested in jobs) for Employeers (they will search)
+ * Employeers can send out bulk messages to selected users
+ */
+
+(function() {
+
+    angular.module('ezeidApp').controller('ResumeJobSeekerCtrl', [
+        '$rootScope',
+        '$scope',
+        '$http',
+        '$q',
+        '$timeout',
+        'Notification',
+        '$filter',
+        '$window',
+        'GURL',
+        '$interval',
+        'MsgDelay',
+        '$location',
+        '$routeParams',
+        '$route',
+        'GoogleMaps',
+        'UtilityService',
+        function ($rootScope,
+                  $scope,
+                  $http,
+                  $q,
+                  $timeout,
+                  Notification,
+                  $filter,
+                  $window,
+                  GURL,
+                  $interval,
+                  MsgDelay,
+                  $location,
+                  $routeParams,
+                  $route,
+                  GoogleMap,
+                  UtilityService) {
+
+
+
+        }]);
+
+})();
