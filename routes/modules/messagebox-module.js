73a0c08cf99deb46ee01b942e6f0fd981e253ab0
gowrishankar <gowrishankar@hirecraft.in>
2015-07-31 12:25:39 +0530

    added fnSendMessageRequest method and fix logout error.

--- routes/modules/messagebox-module.js
+++ routes/modules/messagebox-module.js
@@ -271,7 +271,7 @@ MessageBox.prototype.validateGroupName = function(req,res,next){
 
 /**
  * @todo FnUpdateUserResponse
- * Method : GET
+ * Method : PUT
  * @param req
  * @param res
  * @param next
@@ -397,7 +397,7 @@ MessageBox.prototype.updateUserResponse = function(req,res,next){
 
 /**
  * @todo FnUpdateUserRelationship
- * Method : GET
+ * Method : PUT
  * @param req
  * @param res
  * @param next
@@ -524,7 +524,7 @@ MessageBox.prototype.updateUserRelationship = function(req,res,next){
 
 /**
  * @todo FnDeleteGroup
- * Method : GET
+ * Method : DELETE
  * @param req
  * @param res
  * @param next
@@ -601,6 +601,132 @@ MessageBox.prototype.deleteGroup = function(req,res,next){
         }
     }
 };
+
+
+
+/**
+ * @todo FnSendMessageRequest
+ * Method : POST
+ * @param req
+ * @param res
+ * @param next
+ * @description api code for send message request
+ */
+MessageBox.prototype.sendMessageRequest = function(req,res,next){
+
+    var _this = this;
+
+    var token  = req.body.token;
+    var groupName  = req.body.group_name;
+    var groupType  = req.body.group_type;
+    var relationType = req.body.relation_type;
+    var userID = req.body.user_id ? req.body.user_id : 0;
+
+    var responseMessage = {
+        status: false,
+        error: {},
+        message: '',
+        data: null
+    };
+
+    var validateStatus = true, error = {};
+
+    if(!token){
+        error['token'] = 'Invalid token';
+        validateStatus *= false;
+    }
+    if(!groupName){
+        error['groupName'] = 'Invalid groupName';
+        validateStatus *= false;
+    }
+    if(!groupType){
+        error['groupName'] = 'Invalid groupName';
+        validateStatus *= false;
+    }
+
+    if(!validateStatus){
+        responseMessage.error = error;
+        responseMessage.message = 'Please check the errors';
+        res.status(400).json(responseMessage);
+    }
+    else {
+        try {
+            st.validateToken(token, function (err, result) {
+                if (!err) {
+                    if (result) {
+                        var queryParams = st.db.escape(groupName) + ',' + st.db.escape(token) + ',' + st.db.escape(groupType)
+                            + ',' + st.db.escape(aboutGroup) + ',' + st.db.escape(relationType) + ',' + st.db.escape(userID);
+                        var query = 'CALL pSendMessageRequest(' + queryParams + ')';
+                        console.log(query);
+                        st.db.query(query, function (err, insertResult) {
+                            console.log(insertResult);
+                            if (!err) {
+                                if (insertResult) {
+
+                                    responseMessage.status = true;
+                                    responseMessage.error = null;
+                                    responseMessage.message = 'Message Request send successfully';
+                                    responseMessage.data = {
+                                        token: req.body.token,
+                                        groupName: req.body.group_name,
+                                        groupType: req.body.group_type,
+                                        relationType: req.body.relation_type,
+                                        userID: req.body.user_id
+                                    };
+                                    res.status(200).json(responseMessage);
+                                    console.log('FnSendMessageRequest: Message Request send successfully');
+                                }
+                                else {
+                                    responseMessage.message = 'Message Request not send';
+                                    responseMessage.error = null;
+                                    res.status(200).json(responseMessage);
+                                    console.log('FnSendMessageRequest:Message Request not send');
+                                }
+                            }
+
+                            else {
+                                responseMessage.message = 'An error occured ! Please try again';
+                                responseMessage.error = {
+                                    server: 'Internal Server Error'
+                                };
+                                res.status(500).json(responseMessage);
+                                console.log('FnSendMessageRequest: error in Message Request :' + err);
+                            }
+                        });
+                    }
+                    else {
+                        responseMessage.message = 'Invalid token';
+                        responseMessage.error = {
+                            token: 'Invalid Token'
+                        };
+                        responseMessage.data = null;
+                        res.status(401).json(responseMessage);
+                        console.log('FnSendMessageRequest: Invalid token');
+                    }
+                }
+                else {
+                    responseMessage.error = {
+                        server: 'Internal Server Error'
+                    };
+                    responseMessage.message = 'Error in validating Token';
+                    res.status(500).json(responseMessage);
+                    console.log('FnSendMessageRequest:Error in validating Token' + err);
+                }
+            });
+        }
+        catch (ex) {
+            responseMessage.error = {
+                server: 'Internal Server Error'
+            };
+            responseMessage.message = 'An error occurred !'
+            res.status(500).json(responseMessage);
+            console.log('Error : FnSendMessageRequest ' + ex.description);
+            var errorDate = new Date();
+            console.log(errorDate.toTimeString() + ' ......... error ...........');
+        }
+    }
+};
+
 
 
 module.exports = MessageBox;
