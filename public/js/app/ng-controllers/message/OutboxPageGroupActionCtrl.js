bf055a737874b0afeea7cb0cac2d34c6c32eef9e
sandeep_gantait <sandeep_gantait>
2015-08-11 11:19:45 +0530

    messgaing: chat design complete

--- public/js/app/ng-controllers/message/OutboxPageGroupActionCtrl.js
+++ public/js/app/ng-controllers/message/OutboxPageGroupActionCtrl.js
@@ -408,24 +408,15 @@ angular.module('ezeidApp').
                 $('#ezeone-id').val(ezeone);
 
                 validateGroupMember(ezeone,$scope.activeGroupId).then(function(data){
-                    if(data.status == -1)
-                    {
-                        /* enable add btn */
-                    }
-                    else
-                    {
-                        /* disable sadd btn */
-                    }
-
 
                     if(data.userstatus && data.userstatus == -1)
                     {
                         /* Group name is Unique: passed the validity test! */
                         ezeOneValidationAction(1);
-                        $scope.activeEzeOneId = resp.data[0].masterid;
-                        $scope.activeEzeOneName = resp.data[0].name;
+                        $scope.activeEzeOneId = data.masterid;
+                        $scope.activeEzeOneName = data.name;
                     }
-                    else if(resp.data[0].userstatus && resp.data[0].userstatus == -2)
+                    else if(data.userstatus && data.userstatus == -2)
                     {
                         /* EZEONE does not exists */
                         ezeOneValidationAction(2);
