d56b32bbd3e792352f1d85497890446938a315b0
indrajeet <indrajeet@hirecraft.in>
2015-08-14 15:53:50 +0530

    Sales Report preparation done and bind testing remaining

--- public/js/app/ng-controllers/business-manager/sales/SalesReportCtrl.js
+++ public/js/app/ng-controllers/business-manager/sales/SalesReportCtrl.js
@@ -33,9 +33,67 @@
                   GoogleMap,
                   UtilityService) {
 
-            $scope.reportFromDate = '';
-            $scope.reportToDate = '';
+            var todayDate = moment().format('DD-MMM-YYYY hh:mm A');
+            var dateOneMonthBefore = moment(todayDate,'DD-MMM-YYYY hh:mm A').subtract(1,'month').format('DD-MMM-YYYY hh:mm A');
 
-            
+            var reportData = null;
+
+            $scope.loadReportData = function(fromDate,toDate,stages,probabilities,userIds){
+                $http({
+                    url : GURL + 'sales_statistics',
+                    method : 'GET',
+                    params : {
+                        token : $rootScope._userInfo.Token,
+                        from_date : '',
+                        to_date : '',
+                        stages : '',
+                        probabilities : '',
+                        user : ''
+                    }
+                }).success(function(resp,statusText,statusCode){
+                    if(resp){
+                        if(resp.status){
+                            if(resp.data){
+                                
+                            }
+                        }
+                        else{
+                            Notification.error({
+                                title : 'Error',
+                                message : 'An error occurred! Please try again',
+                                delay : MsgDelay
+                            });
+                        }
+                    }
+                    else{
+                        Notification.error({
+                            title : 'Error',
+                            message : 'An error occurred! Please try again',
+                            delay : MsgDelay
+                        });
+                    }
+
+                }).error(function(err,statusText,statusCode){
+                    if(!statusCode){
+                        Notification.error({
+                            title : 'Connection Lost',
+                            message : 'Unable to reach server ! Please check your connection',
+                            delay : MsgDelay});
+                    }
+                    else{
+                        Notification.error({
+                            title : 'Error',
+                            message : 'An error occurred! Please try again',
+                            delay : MsgDelay
+                        });
+                    }
+                });
+            };
+
+
+            $scope.reportFromDate = dateOneMonthBefore;
+            $scope.reportToDate = todayDate;
+
+
         }])
 })();
