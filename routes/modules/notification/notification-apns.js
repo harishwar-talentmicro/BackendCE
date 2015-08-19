196a38a1aca741652367d77db4d3a42f6a57a6f1
indrajeet <indrajeet@hirecraft.in>
2015-08-19 09:18:58 +0530

    Messaging module finished off with functions Functional calls for queue creation remaining

--- routes/modules/notification/notification-apns.js
+++ routes/modules/notification/notification-apns.js
@@ -1,3 +1,60 @@
+
+function AppleNotification(){
+
+};
+
+
 /**
- * Created by EZEID on 8/13/2015.
+ * Payload is a well formed message in JSON format (already mentioned in document properly)
+ * @param iphoneId
+ * @param payload (JSON Object)
  */
+AppleNotification.prototype.sendAppleNS = function(iphoneId,payload){
+    var http = require('http');
+    var apn = require('apn');
+
+    var myPhone =  iphoneId;
+
+
+    try{
+        var myDevice = new apn.Device(myPhone);
+
+
+        var note = new apn.Notification();
+        note.badge = 2;
+        note.sound = "notification-beep.wav";
+        note.alert = { "body" : payload.g_title +' : ' +payload.message, "action-loc-key" : "Play" ,
+            "launch-image" :
+                "mysplash.png","JsonResult":JSON.stringify(payload)};
+        note.payload = payload;
+
+        note.device = myDevice;
+
+        var callback = function(errorNum, notification){
+            console.log('Error is: %s', errorNum);
+            console.log("Note " + notification);
+        };
+        var options = {
+            gateway: 'gateway.sandbox.push.apple.com', // this URL is different for Apple's Production Servers and changes when you go to production
+            errorCallback: callback,
+            cert: 'cert.pem',
+            key:  'key.pem',
+            passphrase: 'hire@123',
+            port: 2195,
+            enhanced: true,
+            cacheLength: 100
+        };
+            var apnsConnection = new apn.Connection(options);
+            apnsConnection.pushNotification(note, myDevice);
+    }
+    catch(ex){
+        console.log(ex);
+        console.log('An error occurred while sending apple push notification');
+        console.log('Payload For Apple Push Notification : '+JSON.stringify(payload));
+    }
+
+
+};
+
+
+module.exports = AppleNotification;
