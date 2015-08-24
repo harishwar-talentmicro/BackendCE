320fcc55f2f46c842c1250bee80f94cf858d9153
indrajeet <indrajeet@hirecraft.in>
2015-08-24 18:32:38 +0530

    Fixing bug with action type and status type loading Creating new templates for location and user details for wizard style profile

---
+++ public/js/app/ng-controllers/profile/user/ProfileUserCtrl.js
@@ -0,0 +1,35 @@
+angular.module('ezeidApp').controller('ProfileUserCtrl',[
+    '$rootScope',
+    '$scope',
+    '$http',
+    '$q',
+    '$timeout',
+    'Notification',
+    '$filter',
+    '$window',
+    'GURL',
+    '$interval',
+    'ScaleAndCropImage',
+    'MsgDelay',
+    '$location',
+    '$routeParams',
+    function (
+        $rootScope,
+        $scope,
+        $http,
+        $q,
+        $timeout,
+        Notification,
+        $filter,
+        $window,
+        GURL,
+        $interval,
+        ScaleAndCropImage,
+        MsgDelay,
+        $location,
+        $routeParams
+    ) {
+
+
+    }
+]);
