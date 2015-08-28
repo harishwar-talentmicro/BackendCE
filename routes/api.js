731c9547c99bee1d40821fa3886282cff23e4804
indrajeet <indrajeet@hirecraft.in>
2015-08-28 18:06:47 +0530

    Password reset api's and views for front end Functionality tested and verified

--- routes/api.js
+++ routes/api.js
@@ -44,7 +44,7 @@ router.get('/ewtGetDocument', LocationManager.FnGetDocument);
 router.post('/ewtSaveDoc', LocationManager.FnSaveDoc);
 router.get('/ewtGetAccessHistory', LocationManager.FnGetAccessHistory);
 router.post('/ewtForgetPassword', LocationManager.FnForgetPassword);
-router.post('/pass_reset_code',LocationManager.FnVerifyResetPasswordLink);
+router.get('/pass_reset_code',LocationManager.FnVerifyResetPasswordLink);
 router.post('/verify_secret_code',LocationManager.FnVerifySecretCode);
 router.get('/ewtDecryptPassword', LocationManager.FnDecryptPassword);
 router.post('/ewtChangePassword', LocationManager.FnChangePassword);
